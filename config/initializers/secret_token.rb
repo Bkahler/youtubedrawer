ad7d923f1e91ca3d7240b6242944c2082394c24c
Bryan K <bkahler3@gmail.com>
2013-12-13 09:46:48 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tubedrawers::Application.config.secret_token = '79aaf25b2c817236b587adb23da21ac971de309ca17b214dbfa7260ff6b7b731d61371b158785a3fa4ae37218dcef1d53665bbe21b91c734d93f28e34aad0058'
