9748328e368d057646f26a6952586c989059f9cc
Bryan K <bkahler3@gmail.com>
2013-12-15 13:53:32 -0800

    colorbox

---
+++ app/assets/javascripts/tubes.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  alert('loaded')
+
+  $('.video_layer').colorbox(
+      {iframe:true,
+       speed:1000,
+       opacity:1,
+       returnFocus:true,
+       // width:'80%',
+       // height:'80%',
+       innerWidth: '600px',
+        innerHeight: '600px',
+
+        });
+
+})
+
+
+
