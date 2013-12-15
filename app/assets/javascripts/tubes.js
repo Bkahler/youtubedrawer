80c011a128d5a6ca35aaa4294d26aa7171389d93
Bryan K <bkahler3@gmail.com>
2013-12-15 14:40:15 -0800

    remove bootstrap:

--- app/assets/javascripts/tubes.js
+++ app/assets/javascripts/tubes.js
@@ -2,17 +2,19 @@ $(function(){
 
   alert('loaded')
 
-  $('.video_layer').colorbox(
-      {iframe:true,
-       speed:1000,
-       opacity:1,
-       returnFocus:true,
-       // width:'80%',
-       // height:'80%',
-       innerWidth: '600px',
-        innerHeight: '600px',
+  // $('.video_layer').colorbox(
+  //     {iframe:true,
+  //      speed:1000,
+  //      opacity:1,
+  //      returnFocus:true,
+  //      // width:'80%',
+  //      // height:'80%',
+  //      innerWidth: '600px',
+  //       innerHeight: '600px',
 
-        });
+  //       });
+
+
 
 })
 
