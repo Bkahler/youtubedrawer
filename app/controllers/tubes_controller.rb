08c18c886dec50d9625cfbd718629a3dc3df74ca
Bryan K <bkahler3@gmail.com>
2013-12-15 15:49:40 -0800

    setup playlist creation method

--- app/controllers/tubes_controller.rb
+++ app/controllers/tubes_controller.rb
@@ -4,9 +4,17 @@ class TubesController < ApplicationController
 
   def search
     query = params[:search]
+
     client = YouTubeIt::Client.new(:dev_key => ENV["YOUTUBE_API"])
     @results = client.videos_by(:query => query, :page => 1, :per_page => 10)
+
     render :index
+  end
+
+  def create
+     @playlist = Playlist.create(title:params[:playlist], user_id:params[:id])
+
+     render :index
   end
 
 end
