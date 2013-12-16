2a201d3df7d01a495d35af5ecfd925d96646ab85
Bryan K <bkahler3@gmail.com>
2013-12-16 09:31:02 -0800

    Added isotope

--- app/controllers/tubes_controller.rb
+++ app/controllers/tubes_controller.rb
@@ -3,18 +3,22 @@ class TubesController < ApplicationController
   end
 
   def search
+    @current_user = current_user
     query = params[:search]
-
     client = YouTubeIt::Client.new(:dev_key => ENV["YOUTUBE_API"])
     @results = client.videos_by(:query => query, :page => 1, :per_page => 10)
-
     render :index
   end
 
   def create
      @playlist = Playlist.create(title:params[:playlist], user_id:params[:id])
-
      render :index
+  end
+
+  def addtoPlaylist
+    @video = Video.create(title:params[:title],thumbnail:params[:thumbnail],embed_url:params[:embed_url])
+    @playlistVideo = PlaylistVideo.create(playlist_id:params[:playlist_id], video_id:@video.id)
+    render :index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Tubedrawers::Application.routes.draw do
   resources :tubes
 
   get '/search' => 'tubes#search'
+  post '/addTo' => 'tubes#addtoPlaylist'
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 end
